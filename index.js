1c6f4fff9b1fac6fc0623086b59f1b620135404b
MikelCruz <mikel.cruz@ikasle.aeg.eus>
2023-09-18 19:55:06 +0200

    Creacion de Apartado de funciones y comienzo de limpieza del array recibido por el fetch

--- index.js
+++ index.js
@@ -1,18 +1,35 @@
 
 
 const getAllDonuts = async () => {
-    
     return fetch('https://gist.githubusercontent.com/Oskar-Dam/62e7175dc542af53a9d18cb292422425/raw/a6cce2b68ea13a77ec5ea7bdfb4df8f23f9ae95f/donuts.json')
     .then(response => response.json())
 }
 
 const fetchAsyncDonuts = async() => {
     try {
-        const result = await getAllDonuts()
-        console.log(result)
+        const getResult = await getAllDonuts()
+        const allDonuts = getResult.items.item
+
+        showTheHighestSugarDonut(allDonuts);
+
+
     } catch (error){
         console.log(error.message)
     }
 }
 
 fetchAsyncDonuts();
+
+
+
+
+// ==========================================
+//               FUNCIONES 
+// ==========================================
+
+
+function showTheHighestSugarDonut(allDonuts)
+{
+    
+    
+}
